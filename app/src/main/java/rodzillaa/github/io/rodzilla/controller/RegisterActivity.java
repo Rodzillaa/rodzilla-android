893ba56b623f934dbd7ac7d251726a933c0b9bb5
JamieLee629 <jamie.lee@gatech.edu>
2017-09-28 14:40:24 -0400

    Registration screen

--- app/src/main/java/rodzillaa/github/io/rodzilla/controller/RegisterActivity.java
+++ app/src/main/java/rodzillaa/github/io/rodzilla/controller/RegisterActivity.java
@@ -1,15 +1,39 @@
 package rodzillaa.github.io.rodzilla.controller;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import rodzillaa.github.io.rodzilla.R;
 
 public class RegisterActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.registerlayout);
+
+        Button registerButton = (Button) findViewById(R.id.registerButton);
+        Button cancelButton = (Button) findViewById(R.id.cancelButton);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //REGISTRATION
+                //Code to input user info into database
+            }
+        });
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                //Cancel Registration and direct user back to welcome page
+                Intent cancelIntent = new Intent(getBaseContext(), WelcomeActivity.class);
+                startActivity(cancelIntent);
+            }
+        });
     }
 }
