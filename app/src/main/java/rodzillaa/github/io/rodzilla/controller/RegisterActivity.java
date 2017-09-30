c17135bdcac88b5feb4fb76edcd383d6c9c858f1
JamieLee629 <jamie.lee@gatech.edu>
2017-09-30 19:02:30 -0400

    added options to Account Type spinner

--- app/src/main/java/rodzillaa/github/io/rodzilla/controller/RegisterActivity.java
+++ app/src/main/java/rodzillaa/github/io/rodzilla/controller/RegisterActivity.java
@@ -4,8 +4,10 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.RadioButton;
+import android.widget.Spinner;
 
 import rodzillaa.github.io.rodzilla.R;
 
@@ -19,6 +21,7 @@ public class RegisterActivity extends AppCompatActivity {
 
         Button registerButton = (Button) findViewById(R.id.registerButton);
         Button cancelButton = (Button) findViewById(R.id.cancelButton);
+        Spinner accountTypeSpinner = (Spinner) findViewById(R.id.userAdminspinner);
 
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,6 +39,10 @@ public class RegisterActivity extends AppCompatActivity {
                 startActivity(cancelIntent);
             }
         });
+        String[] accountTypes = {"User","Admin"};
+        ArrayAdapter<String> adapter = new ArrayAdapter(this,android.R.layout.simple_spinner_item, accountTypes);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        accountTypeSpinner.setAdapter(adapter);
 
 
     }
