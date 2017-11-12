24111ba83a48004d790ed9ed63338e4b46494633
JamieLee629 <jamie.lee@gatech.edu>
2017-11-12 12:55:10 -0500

    deleting empty ratSightingsFragment file

--- app/src/main/java/rodzillaa/github/io/rodzilla/controller/RatSightingListFragment.java
+++
@@ -1,9 +0,0 @@
-package rodzillaa.github.io.rodzilla.controller;
-
-import android.app.ListFragment;
-
-/**
- * Fragment for Rat Sighting List.
- */
-public class RatSightingListFragment extends ListFragment {
-}
